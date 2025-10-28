4f0cda509e6cc01660271e089aa4eec66a59bdc7
Azeez Ibrahim <139223601+Markzeez@users.noreply.github.com>
2025-10-28 13:02:45 +0100

    new

---
+++ src/data.ts
@@ -0,0 +1,47 @@
+export const slides = [
+    {
+        src: "https://res.cloudinary.com/dyjo2mvqb/image/upload/v1713320465/pexels-farhad-irani-17893793_os9q1z.jpg",
+        title: "Slide Title One",
+        description: 'Slide Description\n\nthis is new line'
+    },
+    {
+        src:  "https://res.cloudinary.com/dyjo2mvqb/image/upload/v1713320463/pexels-valentin-angel-fernandez-17932141_p0gmqp.jpg",
+        title: "Slide Title Two",
+        description: 'Slide Description Two'
+    },
+    {
+        src:  "https://res.cloudinary.com/dyjo2mvqb/image/upload/v1713320260/pexels-hicret-16953646_dycu7f.jpg",
+        title: "Slide Title Three",
+        description: 'Slide Description Three'
+    },
+    {
+        src: "https://res.cloudinary.com/dyjo2mvqb/image/upload/v1713320246/pexels-ilia-bronskiy-20888058_mrbxx4.jpg",
+        title: "Slide Title Four",
+        description: 'Slide Description Four'
+    },
+    {
+        src:  "https://res.cloudinary.com/dyjo2mvqb/image/upload/v1713320207/pexels-line-knipst-20699318_jywkmg.jpg",
+        title: "Slide Title Five",
+        description: 'Slide Description Five'
+    },
+    {
+        src: "https://res.cloudinary.com/dyjo2mvqb/image/upload/v1713321323/pexels-feyza-da%C5%9Ftan-11849914_ctmxju.jpg",
+        title: "Slide Title Six",
+        description: 'Slide Description Six'
+    },
+    {
+        src: "https://res.cloudinary.com/dyjo2mvqb/image/upload/v1713320183/pexels-photo-21345810_swtroy.jpg",
+        title: "Slide Title Seven",
+        description: 'Slide Description Seven'
+    },
+    {
+        src:"https://res.cloudinary.com/dyjo2mvqb/image/upload/v1713320160/pexels-vlado-paunovic-20584635_umozxv.jpg",
+        title: "Slide Title Seven",
+        description: 'Slide Description Seven'
+    },
+    {
+        src: "https://res.cloudinary.com/dyjo2mvqb/image/upload/v1713320145/pexels-hissetmehurriyeti-11697260_bhrlto.jpg",
+        title: "Slide Title Eight",
+        description: 'Slide Description Eight'
+    },
+]
